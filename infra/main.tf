a89a919b0aca4bdb55899efb340ccfd77f5fc3ef
aminespinoza10 <amin.espinoza@outlook.com>
2025-04-21 16:41:31 -0500

    Terraform agregado

---
+++ infra/main.tf
@@ -0,0 +1,35 @@
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg_name.id
+  location = var.resource_group_location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = true
+}
+
+resource "azurerm_log_analytics_workspace" "log_analytics" {
+  name                = "${var.resource_group_name_prefix}-law"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_container_app_environment" "apps_environment" {
+  name                       = var.container_app_env_name
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
+}
